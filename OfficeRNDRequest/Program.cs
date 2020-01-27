d03e90a4be7a8e2af544671b57dd038df1245b74
IHavePortableBrain <vlad.krs7@gmail.com>
2020-01-27 17:32:07 +0300

    Add project files.

---
+++ OfficeRNDRequest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OfficeRNDRequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
