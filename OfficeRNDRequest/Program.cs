9c4b81de2178ac61d2fe6472203cd34fac536ecd
IHavePortableBrain <vlad.krs7@gmail.com>
2020-01-27 18:51:32 +0300

    Add: parse access token from response

--- OfficeRNDRequest/Program.cs
+++ OfficeRNDRequest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using RestSharp;
 using System;
+using Newtonsoft.Json;
 
 namespace OfficeRNDRequest
 {
@@ -16,6 +17,16 @@ namespace OfficeRNDRequest
             request.AddParameter("grant_type", "client_credentials");
             request.AddParameter("scope", "officernd.api.read officernd.api.write");
             IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+
+            client.BaseUrl = new Uri("https://app.officernd.com/i/organizations/vsgate-officernd-trial/resources");
+            request = new RestRequest(Method.GET);
+            dynamic responseBodyParsed = JsonConvert.DeserializeObject(response.Content);
+
+            Console.WriteLine(responseBodyParsed.access_token);
+            request.AddHeader("Authorization", responseBodyParsed.access_token);
+
+            response = client.Execute(request);
             Console.WriteLine(response.Content);
 
             Console.Read();
